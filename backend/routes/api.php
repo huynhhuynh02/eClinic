26e5e049f26fbe7c2ba7ff3e9fa674a32bbef046
Rikai_Duy.Duong <duy.duong@rikai.technology>
2021-11-25 13:58:31 +0700

    Auth api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', 'API\AuthController@register');
+
+Route::post('login', 'API\AuthController@login')->name('login');
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // API route for logout user
+    Route::get('profile', 'API\AuthController@profile');
+    Route::post('logout', 'API\AuthController@logout');
 });
